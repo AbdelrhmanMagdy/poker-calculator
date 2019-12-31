ddf8be5b3a4ef19d600134b81bcedfaa2badfce6
Abdelrhman Magdy <a-abmag@microsoft.com>
2019-12-31 03:46:27 +0200

    fixing bugs

---
+++ build/precache-manifest.c90398e0d8bd0c5684ec3087bba0df78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8e235f365dd3155786ea1a76b5f13f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9617c27fdd73a7c25462",
+    "url": "./static/js/2.759e49a8.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "./static/js/2.759e49a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d170cd0a860d943d581d",
+    "url": "./static/js/main.1df0ad50.chunk.js"
+  },
+  {
+    "revision": "90f9ec8fcca2435426f1",
+    "url": "./static/js/runtime-main.f5681265.js"
+  }
+]);
